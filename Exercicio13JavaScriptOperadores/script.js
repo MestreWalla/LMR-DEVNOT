77e1fd3824131f3d2d767d44fe5cfbea1f49e39d
MestreWalla <mayconvitorcorrea@gmail.com>
2023-05-04 21:36:52 -0300

    update

--- Exercicio13JavaScriptOperadores/script.js
+++ Exercicio13JavaScriptOperadores/script.js
@@ -1,3 +1,3 @@
 let NOME;
-    Nome = prompt('Entre com seu nome: ','Digite-o aqui');
-    document.write('Oi '+ NOME + ' esteja a contade.');
+    NOME = prompt('Entre com seu nome: ','Digite-o aqui');
+    document.write('Oi '+ NOME + ' esteja a vontade.');
---
+++ Exercicio13JavaScriptOperadores/script1.js
@@ -0,0 +1,11 @@
+//Script de valor inteiro
+let numero1;
+let numero2;
+let resposta;
+
+numero1 = prompt('Digite um valor: ' , 'Digite um Número');
+numero2 = prompt('Digite outro valor: ' , 'Digite um Número');
+//realiza a soma de dois nº inteiros
+respposta = parseInt(numero1) + parseInt(numero2);
+
+document.write('O resultado da Soma é ', respposta);
