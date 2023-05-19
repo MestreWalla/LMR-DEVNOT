80cc03c721874ec6c23b0378a9dbc55a74f108f4
MestreWalla <mayconvitorcorrea@gmail.com>
2023-05-18 21:18:31 -0300

    update

--- Exercicio16JavaScriptFormulario/script.js
+++ Exercicio16JavaScriptFormulario/script.js
@@ -1,39 +1,77 @@
-function completName() {
+function completeName() {
   let firstName = document.getElementById("fName").value;
-  let operator = document.getElementById("sName").value;
+  let secondName = document.getElementById("sName").value;
   let lastName = document.getElementById("lName").value;
 
-  document.write(
-    "Your complete name is: " + firstName + " " + secondName + " " + lastName
-  );
+  document.write("Your complete name is: " + firstName + " " + secondName + " " + lastName);
   alert("Welcome: " + firstName + " " + secondName + " " + lastName);
 }
 
 function result() {
-  let firstNumber = document.getElementById("fNumber").value;
+  let firstNumber = document.getElementById("fNumber").value.replace(",", ".");
   let operator = document.getElementById("operator").value;
-  let secondNumber = document.getElementById("sNumber").value;
+  let secondNumber = document.getElementById("sNumber").value.replace(",", ".");
 
   switch (operator) {
     case "+":
-      alert("RESULTADO: " + parseInt(firstNumber) + parseInt(secondNumber));
-      break;
-    case "-":
-      alert("RESULTADO: " + parseInt(firstNumber) - parseInt(secondNumber));
-      break;
-    case "*":
-      alert("RESULTADO: " + parseInt(firstNumber) * parseInt(secondNumber));
-      break;
-    case "/":
-      if (secondNumber == 0) {
-        alert("Invalid");
+      if (firstNumber == "" || secondNumber == "") {
+        alert("Write a valid number");
       } else {
-        alert(
-          "RESULTADO: " + parseFloat(firstNumber) / parseFloat(secondNumber)
-        );
-      }
-      break;
+        alert("RESULTADO: " + (parseFloat(firstNumber) + parseFloat(secondNumber)));
+      } break;
+    case "-":
+      if (firstNumber == "" || secondNumber == "") {
+        alert("Write a valid number");
+      } else {
+        alert("RESULTADO: " + (parseFloat(firstNumber) - parseFloat(secondNumber)));
+      } break;
+    case "*":
+      if (firstNumber == "" || secondNumber == "") {
+        alert("Write a valid number");
+      } else {
+        alert("RESULTADO: " + parseFloat(firstNumber) * parseFloat(secondNumber));
+      }  break;
+    case "/":
+      if (secondNumber == "" || firstNumber == "") {
+        alert("Write a valid number");
+      } else if (firstNumber == 0 ||secondNumber == 0) {
+        alert("0 is not valid for division")
+      } else {
+        alert("RESULTADO: " + parseFloat(firstNumber) / parseFloat(secondNumber));
+      } break;
     default:
       alert("Choose a valid operation");
+  }
+}
+
+function login() {
+  let uName = document.getElementById("uName").value;
+  let eMail = document.getElementById("eMail").value;
+  let fPass = document.getElementById("fPass").value;
+  let sPass = document.getElementById("sPass").value;
+  if (fPass == "" || sPass == "" || eMail == "" || uName == "") {
+    alert("Still blank fields");
+  } else if ( fPass != sPass){
+    alert("Write a valid password");
+  } else {
+    alert("Correct password");
+  }
+}
+
+function register() {
+  let uName = document.getElementById("uName").value;
+  let eMail = document.getElementById("eMail").value;
+  let fPass = document.getElementById("fPass").value;
+  let sPass = document.getElementById("sPass").value;
+  if (fPass == "" || sPass == "") {
+    alert("Password is blank");
+  } else if ( fPass != sPass){
+    alert("Write a valid password");
+  } else if (uName == ""){
+    alert("Name is blank");
+  } else if (eMail == ""){
+    alert("E-mail is blank");
+  } else {
+    alert("Correct password");
   }
 }
