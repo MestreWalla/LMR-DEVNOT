9ccdc292c8f5714902c4169b3a97b699048412a0
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-09-20 17:25:01 -0300

    update

--- Semestre02-PWFE/04SituaçaodeAprendisagem02/js/lampada.js
+++ Semestre02-PWFE/04SituaçaodeAprendisagem02/js/lampada.js
@@ -6,13 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const chave = document.getElementById("chave");
     const music = new Audio('sound/interruptor.mp3');
     let backgroundColor = "yellow";
-    var contador = 1;
+    let contador = 1;
 
     function quarto() {
         if (backgroundColor == "yellow" && contador < 5) {
             luz.style.backgroundColor = "black";
             luz.style.boxShadow = "none";
             lampada.style.backgroundColor = "rgb(102, 3, 3)";
+            lampada.style.filter = "brightness(30%)";
             backgroundColor = "black";
             quebrado.style.display = "none";
             chave.style.display = "none";
@@ -22,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
             luz.style.backgroundColor = "yellow";
             luz.style.boxShadow = "0px 0px 90px 30px rgb(253, 250, 162)";
             lampada.style.backgroundColor = "brown";
+            lampada.style.filter = "brightness(100%)";
             backgroundColor = "yellow";
             quebrado.style.display = "none";
             chave.style.display = "none";
@@ -31,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
             luz.style.backgroundColor = "black";
             luz.style.boxShadow = "none";
             lampada.style.backgroundColor = "rgb(102, 3, 3)";
+            lampada.style.filter = "brightness(30%)";
             quebrado.style.display = "block";
             chave.style.display = "block";
             backgroundColor = "black";
