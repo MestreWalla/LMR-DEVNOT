e458666b96ddceaafbe8867f5e12b5b54864d902
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-10-23 22:28:13 -0300

    update Semestre02-PWFE/09SituacaodeAprendisagem3/js/header.js

--- Semestre02-PWFE/09SituacaodeAprendisagem3/js/header.js
+++ Semestre02-PWFE/09SituacaodeAprendisagem3/js/header.js
@@ -10,11 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
         var scrollPosition = window.scrollY || window.pageYOffset;
         if (scrollPosition >= menuPosition) {
             // Aplique a animação e a cor do menu
-            menu02.style.animation = "showMenu 0.5s forwards";
+            menu02.style.animation = "showMenu 0.5s forwards infinite ease-in-out";
             menu02.style.backgroundColor = "blue";
         } else {
             // Aplique a cor do menu
             menu02.style.backgroundColor = "red";
         }
     });
+});
+
+// Quando o usuário passar o mouse em cima da div
+menu02.addEventListener("mouseenter", function() {
+    // Aplique a animação
+    menu02.style.animation = "showMenu 0.5s forwards infinite ease-in-out";
+});
+
+// Quando o usuário passar o mouse para fora da div
+menu02.addEventListener("mouseleave", function() {
+    // Remova a animação
+    menu02.style.animation = "none";
 });
