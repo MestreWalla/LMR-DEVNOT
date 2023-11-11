89f7960cf8631bee0a579fc8304a332a2237a205
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-10 21:39:56 -0300

    update

---
+++ Semestre02-PWFE/08ProjetoVue/projeto-clima-api/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Clima />
+  </div>
+</template>
+
+<script>
+import Clima from './components/Clima.vue';
+export default {
+  name: 'App',
+
+  components: {
+    Clima,
+  },
+};
+</script>
